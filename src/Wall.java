8eddca69d031b733ff7746a8fde589901c7dc8e7
VikashKumarUgam <vikash.k2@ugamsolutions.com>
2022-02-21 18:58:27 +0530

    Section 7: OOP Part 2 - Composition, Encapsulation, and Polymorphism
    Signed-off-by: VikashKumarUgam <vikash.k2@ugamsolutions.com>

--- src/Wall.java
+++ src/Wall.java
@@ -1,40 +1,11 @@
 public class Wall {
+    private String direction;
 
-    private double width;
-    private double height;
-
-    public Wall() {
+    public Wall(String direction) {
+        this.direction = direction;
     }
 
-    public Wall(double width, double height) {
-        setWidth(width);
-        setHeight(height);
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        if (width < 0){
-            this.width = 0;
-        }
-        else
-            this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        if(height < 0)
-            this.height=0;
-        else
-            this.height = height;
-    }
-
-    public double getArea(){
-        return this.width * this.height;
+    public String getDirection() {
+        return direction;
     }
 }
